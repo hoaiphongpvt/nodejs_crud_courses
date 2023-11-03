99b06e3d91d0f68c7134b4f939e4f0906a871814
hoaiphongpvt <hoaiphongpvt@gmail.com>
2023-11-03 14:42:16 +0700

    Add Prettier

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const morgan = require('morgan');
-const path = require('path');
-const route = require('./routes');
+const express = require('express')
+const exphbs = require('express-handlebars')
+const morgan = require('morgan')
+const path = require('path')
+const route = require('./routes')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(express.urlencoded());
-app.use(express.json());
+app.use(express.urlencoded())
+app.use(express.json())
 
-app.use(morgan('combined'));
+app.use(morgan('combined'))
 
 app.engine(
     'hbs',
     exphbs({
         extname: '.hbs',
-    }),
-);
-app.set('view engine', 'hbs');
-          app.set('views', path.join(__dirname, 'resources\\views'));
+    })
+)
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources\\views'))
 
-route(      app);
+route(app)
 
 app.listen(port, () => {
-              console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
